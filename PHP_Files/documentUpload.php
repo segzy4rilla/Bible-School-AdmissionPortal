e7aa0e96bb4c31774c94f145d7999bc1cb42e057
Adebimpe Godwin <ade.segs@yahoo.com>
2020-06-07 14:39:50 +0100

    Document Upload working with only 5 files uploading atm, bug needs to be fixed with that

--- PHP_Files/documentUpload.php
+++ PHP_Files/documentUpload.php
@@ -1,23 +1,31 @@
 <?php 
-	$targetDirectory = "uploads/";
-	for($x=0; $x < sizeof($_FILES); ++$x){
-		$targetFilePath = $targetDirectory.basename($_FILES["img1[$x]"]["name"]);
-		$imageFileType = strtolower(pathinfo($targetFilePath,PATHINFO_EXTENSION));
-		$validUpload = 0;
-		
-		if(isset($_POST["submit"])){
-			if($imageFileType == "jpg" || $imageFileType == "docx" || $imageFileType == "txt"){
-				$validUpload = 1;
-			}
-		}
-		
-		if($validUpload == 1){
-			if (move_uploaded_file($_FILES["img1[$x]"]["tmp_name"], $targetFilePath)) {
-				echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-			} else {
-				echo "Sorry, there was an error uploading your file.";
-			}
-		}
+	$targetDirectory = "../image_uploads/";
+
+
+        for($x=0; $x < sizeof($_FILES["img1"]); ++$x){
+
+            try {
+                echo $x;
+                $targetFilePath = $targetDirectory . basename($_FILES["img1"]["name"][$x]);
+                $imageFileType = strtolower(pathinfo($targetFilePath, PATHINFO_EXTENSION));
+                $validUpload = 0;
+
+
+                if ($imageFileType == "jpg" || $imageFileType == "docx" || $imageFileType == "png" || $imageFileType == "txt") {
+                    $validUpload = 1;
+                }
+
+
+                if ($validUpload == 1) {
+                    if (move_uploaded_file($_FILES["img1"]["tmp_name"][$x], $targetFilePath)) {
+                        echo "The file " . basename($_FILES["img1"]["name"][$x]) . " has been uploaded.";
+                    } else {
+                        echo "Sorry, there was an error uploading your file.";
+                    }
+                }
+            }catch (Exception $ex){
+
+            }
 	}
 
 ?>
--- PHP_Files/submit_interviewanswers.php
+++ PHP_Files/submit_interviewanswers.php
@@ -36,7 +36,7 @@ try {
   VALUES ('$question_1','$question_2','$question_3','$question_4','testuserid','$question_5','$question_6','$question_7','$question_8','$question_9','$question_10','$question_11','$question_12','$question_13','$question_14','$question_15','$question_16')";
     // use exec() because no results are returned
     $conn->exec($sql);
-    header('Location: ../loginabmtc.html');
+    header('Location: ../applicantdash.html');
 } catch(PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
 }
