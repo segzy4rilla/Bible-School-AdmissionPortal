611ab40141d0611931e46b444a6f578f72f6f649
YungDeiza <aharuna@live.co.uk>
2020-06-07 15:18:10 +0100

    doc upload php bug fixes

--- PHP_Files/documentUpload.php
+++ PHP_Files/documentUpload.php
@@ -1,30 +1,29 @@
 <?php 
-	$targetDirectory = "../image_uploads/";
+	$targetDirectory = "../uploads/";
 
-	for($x=0; $x < sizeof($_FILES["img1"]); ++$x){
-
+	for($x=0; $x < count($_FILES['img1']['name']); ++$x){
 		try {
-			echo $x;
 			$targetFilePath = $targetDirectory . basename($_FILES["img1"]["name"][$x]);
 			$imageFileType = strtolower(pathinfo($targetFilePath, PATHINFO_EXTENSION));
 			$validUpload = 0;
-
 
 			if ($imageFileType == "jpg" || $imageFileType == "docx" || $imageFileType == "png" || $imageFileType == "txt"|| $imageFileType == "jpeg") {
 				$validUpload = 1;
 			}
 
-
 			if ($validUpload == 1) {
 				if (move_uploaded_file($_FILES["img1"]["tmp_name"][$x], $targetFilePath)) {
-					echo "The file " . basename($_FILES["img1"]["name"][$x]) . " has been uploaded.";
+					echo "The file " . basename($_FILES["img1"]["name"][$x]) . " has been uploaded.\n";
 				} else {
-					echo "Sorry, there was an error uploading your file.";
+					echo "Sorry, there was an error uploading your file.\n";
 				}
+			}
+			else {
+				echo "File could not be uploaded due to restricted file type. must be: .jpg, .jpeg, .doxc, .png or .txt \n";
 			}
 		}
 		catch (Exception $ex){
-			echo "Sorry, there was an error uploading your file.";
+			echo "Sorry, there was an error uploading your file.\n";
 		}
 	}
 ?>
