0ccfc3e9074e650db1e63e3a0a1eccb153b5e9a1
YungDeiza <aharuna@live.co.uk>
2020-06-07 14:48:59 +0100

    doc upload php formatting

--- PHP_Files/documentUpload.php
+++ PHP_Files/documentUpload.php
@@ -1,31 +1,30 @@
 <?php 
 	$targetDirectory = "../image_uploads/";
 
+	for($x=0; $x < sizeof($_FILES["img1"]); ++$x){
 
-        for($x=0; $x < sizeof($_FILES["img1"]); ++$x){
-
-            try {
-                echo $x;
-                $targetFilePath = $targetDirectory . basename($_FILES["img1"]["name"][$x]);
-                $imageFileType = strtolower(pathinfo($targetFilePath, PATHINFO_EXTENSION));
-                $validUpload = 0;
-
-
-                if ($imageFileType == "jpg" || $imageFileType == "docx" || $imageFileType == "png" || $imageFileType == "txt") {
-                    $validUpload = 1;
-                }
+		try {
+			echo $x;
+			$targetFilePath = $targetDirectory . basename($_FILES["img1"]["name"][$x]);
+			$imageFileType = strtolower(pathinfo($targetFilePath, PATHINFO_EXTENSION));
+			$validUpload = 0;
 
 
-                if ($validUpload == 1) {
-                    if (move_uploaded_file($_FILES["img1"]["tmp_name"][$x], $targetFilePath)) {
-                        echo "The file " . basename($_FILES["img1"]["name"][$x]) . " has been uploaded.";
-                    } else {
-                        echo "Sorry, there was an error uploading your file.";
-                    }
-                }
-            }catch (Exception $ex){
+			if ($imageFileType == "jpg" || $imageFileType == "docx" || $imageFileType == "png" || $imageFileType == "txt"|| $imageFileType == "jpeg") {
+				$validUpload = 1;
+			}
 
-            }
+
+			if ($validUpload == 1) {
+				if (move_uploaded_file($_FILES["img1"]["tmp_name"][$x], $targetFilePath)) {
+					echo "The file " . basename($_FILES["img1"]["name"][$x]) . " has been uploaded.";
+				} else {
+					echo "Sorry, there was an error uploading your file.";
+				}
+			}
+		}
+		catch (Exception $ex){
+			echo "Sorry, there was an error uploading your file.";
+		}
 	}
-
 ?>
