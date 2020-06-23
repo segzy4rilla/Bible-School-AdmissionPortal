01c227b929258f54303b97ce14db3ab1766a74b2
YungDeiza <aharuna@live.co.uk>
2020-06-23 07:40:55 +0100

    summary table UD

--- summarytable.php
+++ summarytable.php
@@ -67,13 +67,40 @@ $result = $con->query($query);
     <div class="ecaps-page-content">
         <!-- Top Header Area -->
         <header class="top-header-area d-flex align-items-center justify-content-between">
+
             <div class="left-side-content-area d-flex align-items-center">
-                <!-- Mobile Logo -->
-                <div class="mobile-logo mr-3 mr-sm-4">
-                    <a href="applicantdash.html"><img src="ABTMC.png" alt="Mobile Logo"></a>
+                <div class="ecaps-logo" style="width:75px">
+                    <a href="applicantdash.php">
+                        <img class="desktop-logo" style="min-height:70px; min-width:70px; margin:0px" src="ABTMC.png"
+                             alt="Desktop Logo">
+                        <img class="small-logo" src="ABTMC.png" alt="Mobile Logo">
+                    </a>
                 </div>
 
-                <!-- Triggers -->
+                <div id="google_translate_element"></div>
+
+                <script type="text/javascript">
+                    function googleTranslateElementInit() {
+                        new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+                    }
+                </script>
+
+                <script type="text/javascript"
+                        src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+                <!-- Mobile Logo -->
+                <div class="mobile-logo mr-3 mr-sm-4">
+                    <a href="applicantdash.php"><img src="ABTMC.png" alt="Mobile Logo"></a>
+                </div>
+
+            </div>
+
+            <div class="right-side-navbar d-flex align-items-center justify-content-end">
+                <!-- Mobile Trigger -->
+                <div class="right-side-trigger" id="rightSideTrigger">
+                    <i class="fa fa-reorder"></i>
+                </div>
+                <!-- Three line menu button -->
                 <div class="ecaps-triggers mr-1 mr-sm-3">
                     <div class="menu-collasped" id="menuCollasped">
                         <i class="zmdi zmdi-menu"></i>
@@ -82,178 +109,6 @@ $result = $con->query($query);
                         <i class="zmdi zmdi-menu"></i>
                     </div>
                 </div>
-            </div>
-
-            <div class="right-side-navbar d-flex align-items-center justify-content-end">
-                <!-- Mobile Trigger -->
-                <div class="right-side-trigger" id="rightSideTrigger">
-                    <i class="fa fa-reorder"></i>
-                </div>
-
-                <!-- Top Bar Nav -->
-                <ul class="right-side-content d-flex align-items-center">
-                    <!-- Left Side Nav -->
-                    <li class="hide-phone app-search">
-                        <form role="search" class=""><input type="text" placeholder="Search..." class="form-control"> <button type="submit" class="mr-0"><i class="fa fa-search"></i></button></form>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-envelope-o" aria-hidden="true"></i></button>
-
-                        <div class="dropdown-menu dropdown-menu-right">
-                            <!-- Top Message Area -->
-                            <div class="top-message-area">
-                                <!-- Heading -->
-                                <div class="top-message-heading">
-                                    <div class="heading-title">
-                                        <h6>Messages</h6>
-                                    </div>
-                                    <span>07 New</span>
-                                </div>
-                                <div class="message-box" id="messageBox">
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/10.png" alt="">
-                                        <span class="message-text">
-                                                <span>Jhon Lina</span>
-                                                <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, voluptas!</span>
-                                            </span>
-                                    </a>
-                                    <span class="message-heading">New Messages</span>
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/11.png" alt="">
-                                        <span class="message-text">
-                                                <span>Google Ads: You'll get a refund soon</span>
-                                                <span>27 min ago</span>
-                                            </span>
-                                    </a>
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/7.png" alt="">
-                                        <span class="message-text">
-                                                <span>New Feature: HTTP Method Selection</span>
-                                                <span>56 min ago</span>
-                                            </span>
-                                    </a>
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/8.png" alt="">
-                                        <span class="message-text">
-                                                <span>The Complete JavaScript Handbook</span>
-                                                <span>1 hour ago</span>
-                                            </span>
-                                    </a>
-                                    <span class="message-heading">Hot Messages</span>
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/9.png" alt="">
-                                        <span class="message-text">
-                                                <span>New comment: ecaps Template</span>
-                                                <span>2 days ago</span>
-                                            </span>
-                                    </a>
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/10.png" alt="">
-                                        <span class="message-text">
-                                                <span>6-hour video course on Angular</span>
-                                                <span>3 min ago</span>
-                                            </span>
-                                    </a>
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/11.png" alt="">
-                                        <span class="message-text">
-                                                <span>Google Ads: You'll get a refund soon</span>
-                                                <span>27 min ago</span>
-                                            </span>
-                                    </a>
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/12.png" alt="">
-                                        <span class="message-text">
-                                                <span>New Feature: HTTP Method Selection</span>
-                                                <span>56 min ago</span>
-                                            </span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-bell-o" aria-hidden="true"></i> <span class="active-status"></span></button>
-                        <div class="dropdown-menu dropdown-menu-right">
-                            <!-- Top Notifications Area -->
-                            <div class="top-notifications-area">
-                                <!-- Heading -->
-                                <div class="notifications-heading">
-                                    <div class="heading-title">
-                                        <h6>Notifications</h6>
-                                    </div>
-                                </div>
-
-                                <div class="notifications-box" id="notificationsBox">
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/1.png" alt="">
-                                        <span class="message-text">
-                                                <span>New Feature: HTTP Method Selection</span>
-                                                <span>56 min ago</span>
-                                            </span>
-                                    </a>
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/2.png" alt="">
-                                        <span class="message-text">
-                                                <span>Andrew Done Project</span>
-                                                <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, quam.</span>
-                                            </span>
-                                    </a>
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/3.png" alt="">
-                                        <span class="message-text">
-                                                <span>New Feature: HTTP Method Selection</span>
-                                                <span>56 min ago</span>
-                                            </span>
-                                    </a>
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/4.png" alt="">
-                                        <span class="message-text">
-                                                <span>New Feature: HTTP Method Selection</span>
-                                                <span>56 min ago</span>
-                                            </span>
-                                    </a>
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/5.png" alt="">
-                                        <span class="message-text">
-                                                <span>New Feature: HTTP Method Selection</span>
-                                                <span>56 min ago</span>
-                                            </span>
-                                    </a>
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/6.png" alt="">
-                                        <span class="message-text">
-                                                <span>New Feature: HTTP Method Selection</span>
-                                                <span>56 min ago</span>
-                                            </span>
-                                    </a>
-                                    <a href="#" class="dropdown-item">
-                                        <img src="img/member-img/7.png" alt="">
-                                        <span class="message-text">
-                                                <span>New Feature: HTTP Method Selection</span>
-                                                <span>56 min ago</span>
-                                            </span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="img/member-img/4.png" alt=""></button>
-                        <div class="dropdown-menu header-profile dropdown-menu-right">
-                            <!-- User Profile Area -->
-                            <div class="user-profile-area">
-                                <a href="#" class="dropdown-item"><i class="zmdi zmdi-account profile-icon" aria-hidden="true"></i> My profile</a>
-                                <a href="#" class="dropdown-item"><i class="zmdi zmdi-email-open profile-icon" aria-hidden="true"></i> Messages</a>
-                                <a href="#" class="dropdown-item"><i class="zmdi zmdi-brightness-7 profile-icon" aria-hidden="true"></i> Account settings</a>
-                                <a href="#" class="dropdown-item"><i class="ti-unlink profile-icon" aria-hidden="true"></i> Sign-out</a>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
             </div>
         </header>
 
@@ -273,6 +128,7 @@ $result = $con->query($query);
                                             <th>Index</th>
                                             <th>Applicants Name</th>
                                             <th>Nationality</th>
+											<th>Member of UD-OLGC Church</th>
                                             <th>Applicants Form</th>
                                             <th>Interview Test</th>
                                             <th>Uploaded Documents</th>
@@ -289,7 +145,7 @@ $result = $con->query($query);
                                             echo "<td>" . ++$count . "</td>";
                                             echo "<td>" . $row['First_Name'] . " " . $row['Last_Name'] . "</td>";
                                             echo "<td>" . $row['Nationality'] . "</td>";
-
+											echo "<td>" . $row['Church_Part_Of_UD'] . "</td>";
                                             if ($row['Application_Form_Submitted'] == 1) {
                                                 echo "<td>" . "Completed" . "</td>";
                                             } else {
