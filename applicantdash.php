3f408c6852bf2042786fcecf4119c38e206e9a49
YungDeiza <aharuna@live.co.uk>
2020-07-19 20:38:50 +0100

    add zoom link to app dash

--- applicantdash.php
+++ applicantdash.php
@@ -194,6 +194,26 @@ $applicantDetails = $getApplicantDetails->fetch();
 								</div>
 							</a>
                         </div>
+						<div class="col-lg-12"  style="height:30%">
+                            <a href="applicantmeeting.php">
+								<div class="card" style="margin-bottom:15px;height:100%">
+									<!-- Card body -->
+									<div class="card-body">
+										<div class="row">
+											<div class="col">
+												<h5 class='mb-20'>Applicant Meeting</h5>
+											</div>
+											<div class="col-auto">
+												<div class="clint-icon bg-gradient-danger text-white rounded-circle icon-shape">
+													<i class="fa fa-folder"></i>
+												</div>
+											</div>
+										</div>
+										
+									</div>
+								</div>
+							</a>
+                        </div>
                     </div>
                     
 
