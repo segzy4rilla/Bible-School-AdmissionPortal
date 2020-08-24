8ed1bbe23adc68cba92aa58942eccad82ca2bc16
YungDeiza <aharuna@live.co.uk>
2020-08-24 21:35:14 +0100

    quick changes

--- PHP_Files/dbconfig.php
+++ PHP_Files/dbconfig.php
@@ -1,13 +1,13 @@
 <?php
-//Local
-//$servername = "localhost";
-//$username = "root";
-//$password = "";
-//$dbname = "test";
+	//Local
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "test";
 
-//BlueHost
-$servername = "localhost:3306";
-$username = "anagkaz1_wp780";
-$password = "AbMTC2020!!!";
-$dbname = "anagkaz1_wp780";
+	//BlueHost
+	//$servername = "localhost:3306";
+	//$username = "anagkaz1_wp780";
+	//$password = "AbMTC2020!!!";
+	//$dbname = "anagkaz1_wp780";
 ?>
