9335776bd7f70ff6240f19649ff0212b168b847f
YungDeiza <aharuna@live.co.uk>
2020-08-29 17:40:52 +0100

    db config

--- PHP_Files/dbconfig.php
+++ PHP_Files/dbconfig.php
@@ -1,13 +1,13 @@
 <?php
 	//Local
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "test";
+	//$servername = "localhost";
+	//$username = "root";
+	//$password = "";
+	//$dbname = "test";
 
 	//BlueHost
-	//$servername = "localhost:3306";
-	//$username = "anagkaz1_wp780";
-	//$password = "AbMTC2020!!!";
-	//$dbname = "anagkaz1_wp780";
+	$servername = "localhost:3306";
+	$username = "anagkaz1_wp780";
+	$password = "AbMTC2020!!!";
+	$dbname = "anagkaz1_wp780";
 ?>
